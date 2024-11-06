30c6fb9997fb75ca3b1bb2197c03d8f8ef37f1e5
Cauã Diego <tixyelpvd@gmail.com>
2024-11-06 11:12:43 -0300

    feat: implement dynamic command registration from commands directory

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,34 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+import * as fs from 'fs';
+import * as path from 'path';
 import * as vscode from 'vscode';
+import BaseCommand from './BaseCommand';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "vortaik" is now active!');
+function registerCommands(context: vscode.ExtensionContext, dir: string) {
+  const files = fs.readdirSync(dir);
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  const disposable = vscode.commands.registerCommand('vortaik.helloWorld', () => {
-    // The code you place here will be executed every time your command is executed
-    // Display a message box to the user
-    vscode.window.showInformationMessage('Hello World from vortaik!');
-  });
+  for (const file of files) {
+    const fullPath = path.join(dir, file),
+      stat = fs.statSync(fullPath);
 
-  context.subscriptions.push(disposable);
+    if (stat.isDirectory()) {
+      registerCommands(context, fullPath);
+    } else if (fullPath.endsWith('.ts') || fullPath.endsWith(`.js`)) {
+      const Command = require(fullPath).default;
+
+      if (Command && Command.prototype instanceof BaseCommand) {
+        const instance: BaseCommand = new Command();
+
+        const disposable = vscode.commands.registerCommand('vortaik.' + instance.name, instance.run.bind(context));
+        context.subscriptions.push(disposable);
+      }
+    }
+  }
 }
 
-// This method is called when your extension is deactivated
+export function activate(context: vscode.ExtensionContext) {
+  registerCommands(context, path.join(__dirname, 'commands'));
+
+  // todo
+}
+
 export function deactivate() {}
